77d328ee57b3db8db8cf55ff9f905cc83553b838
Jonathan DIki <jonathandiki@outlook.com>
2024-06-18 06:21:12 +0200

    promise commit

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const promesse1 = new promise(
+const promesse1 = new Promise(
     
     (resolve, reject) =>
     {
+        const y = Math.round(Math.random() * 10)
         resolve(2)
 
     }
 )
-promesse1.then((result) => {
-  console.log(result);
-});
+promesse1.then(
+    (result) => { return result * 2 }
+).then(
+    (result) => {return result * 2}
+).then
+    (
+    result => console.log(result)
+)
