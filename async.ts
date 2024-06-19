04666215c7ae52b4a529cfddef196f1a14f7f4f7
Jonathan DIki <jonathandiki@outlook.com>
2024-06-19 03:18:59 +0200

    ajout des fonctions  async await

---
+++ async.ts
@@ -0,0 +1,25 @@
+
+
+async function test() {
+    return 'n'
+}
+
+
+test().then(
+    (value) => { console.log(value)}
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// une foction async retourne tjr une promesse
